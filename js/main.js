8f64a151799e0e42927f4a03841c27c96f9dfa4a
Stefan Djordjevic <stefan_0905@hotmail.com>
2018-05-17 21:07:29 +0200

    Display reply form on bookmark click

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.carousel').carousel({
         interval: 4000
     })
+
+    $(document).on('click', '#quick-reply', function (){
+        $('#reply-it').removeClass('d-none');
+    })
 });
