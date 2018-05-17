8c54f00e0720e3cf204fe9d55370bcc2e5e7f92a
Stefan Djordjevic <stefan_0905@hotmail.com>
2018-05-17 18:55:12 +0200

    Created Post Class

--- admin/includes/post.class.php
+++ admin/includes/post.class.php
@@ -4,7 +4,7 @@
 class Post extends Db_object 
 {
     protected static $db_table = 'posts';
-    protected static $db_table_fields = array('thread_id', 'user_id', 'message', 'created_at', 'updated_at');
+    protected static $db_table_fields = array('id', 'thread_id', 'user_id', 'message', 'created_at', 'updated_at');
     public $id;
     public $thread_id;
     public $user_id;
@@ -16,6 +16,15 @@ class Post extends Db_object
     {
         $this->created_at = date('Y-m-d h:i:sa');
         $this->updated_at = date('Y-m-d h:i:sa');
+    }
+
+    public static function getRelatedPosts($thread_id)
+    {
+    $sql = "SELECT * FROM posts WHERE thread_id = {$thread_id}";
+    $related_posts = self::find_by_query($sql);
+    if(!empty($related_posts))
+        return $related_posts;
+        else return false;
     }
 }
 
