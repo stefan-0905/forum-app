a37c35308d3db704c19b2efb37ebad9ae9d58a0d
Stefan Djordjevic <stefan_0905@hotmail.com>
2018-05-17 18:52:48 +0200

    Created function for generating footer with suited scripts

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -4,3 +4,20 @@ function redirect($location)
 {
     header("Location: {$location}");
 }
+
+function footer($script_array) 
+{
+    echo 
+        "<footer id='main-footer' class='text-center mt-5 p-5 text-light bg-dark'>
+            <p class='m-0'>Copyright &copy; 2018</p>
+        </footer>
+    
+        <script type='text/javascript' src='js/jquery.min.js'></script>
+        <script type='text/javascript' src='js/popper.min.js'></script>
+        <script type='text/javascript' src='js/bootstrap.min.js'></script>";
+    
+    foreach($script_array as $script)
+        echo "<script type='text/javascript' src='" . $script . "'></script>";
+
+    echo "</body></html>";
+}
