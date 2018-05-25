6176050f611e698bb5a056c31c91fa4fe01f95c0
Stefan Djordjevic <stefan_0905@hotmail.com>
2018-05-25 08:38:43 +0200

    Tweeked it to handle new button to close role_changer feature

--- admin/js/role_changer.js
+++ admin/js/role_changer.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
 
     $(".change-role").click(function() {
-        $(this).prev().toggleClass("d-inline-block");
-        if(!$(this).children().hasClass("fa-check"))
+        let role_changer = $(this);
+        $(this).prev().toggleClass("d-inline-block"); // This refers to change-role button
+        if(!$(this).children().hasClass("fa-check")) {
             $(this).children().removeClass('fa-edit').addClass("fa-check");
-            else {
-                $(this).children().removeClass('fa-check').addClass("fa-edit");
-                let select = $(this).prev()[0];
-                let select_value = select.options[select.selectedIndex].value;
-                console.log(select_value);
-                let user_id = $(this).data("id");
-                console.log(user_id);
-                $.ajax({
-                    url:"includes/ajax_code.php",
-                    data:{update_user_role: true, user_id: user_id, role_id: select_value},
-                    type: "POST",
-                    success: function(data) {
-                        if(!data.error) {
-                            //let user = JSON.parse(data);
-                            // console.log(data);
-                            location.reload(true);
-                        }
+            $(this).after("<a class='close-rolechanger text-danger'><i class='fa fa-times'></i></a>");
+            $(document).on('click', '.close-rolechanger', function (){
+                $(this).prev().prev().toggleClass("d-inline-block"); // This refers to .close button
+                $(this).prev().children().removeClass('fa-check').addClass("fa-edit");
+                $(this).remove();
+            })
+        } else {
+            $(this).children().removeClass('fa-check').addClass("fa-edit");
+            let select = $(this).prev()[0];
+            let select_value = select.options[select.selectedIndex].value;
+            console.log(select_value);
+            let user_id = $(this).data("id");
+            console.log(user_id);
+            $.ajax({
+                url:"includes/ajax_code.php",
+                data:{update_user_role: true, user_id: user_id, role_id: select_value},
+                type: "POST",
+                success: function(data) {
+                    if(!data.error) {
+                        //let user = JSON.parse(data);
+                        // console.log(data);
+                        location.reload(true);
                     }
-                });
-            }
+                }
+            });
+        }
     })
 
     
